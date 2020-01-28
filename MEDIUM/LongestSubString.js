a29ebc51427dd6ff1edacd2b684b5120aa59c012
naima shaikh <naimashaikh550@gmail.com>
2020-01-28 21:29:32 +0500

    'solution'

--- MEDIUM/LongestSubString.js
+++ MEDIUM/LongestSubString.js
@@ -1,3 +1,4 @@
+//Brute First Approach
 let lengthOfLongestSubstring = function(s) {
       let count=0;
      
@@ -17,4 +18,27 @@ let lengthOfLongestSubstring = function(s) {
          }
       }
      return count;
+};
+
+//Sliding window 
+let  lengthOfLongestSubstring = function(s) {
+    let count=0;
+    let i=0;
+    let j=0;
+    let set = new Set();
+    let n=s.length;
+    
+    while (i <n &&  j<n){
+        let char = s. charAt(j);
+        if (!set.has(char)){
+            set.add(char);
+            j++;
+            count =Math.max(count, j-i);
+        } else {
+            set.delete(s.charAt(i));
+             i++;
+        }
+    }
+   return count;
+   
 };
