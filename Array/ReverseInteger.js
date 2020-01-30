cb46435a5419a5c9b67e90cb25d6ada35fc05420
naima shaikh <naimashaikh550@gmail.com>
2020-01-30 18:15:26 +0500

    'solution'

---
+++ Array/ReverseInteger.js
@@ -0,0 +1,11 @@
+let reverse =function(x){
+    return(
+        parseFloat(
+            x
+            .toString()
+            .split('')
+            .reverse()
+            .join('')
+            ) * Math.sign(x)
+    )
+}
