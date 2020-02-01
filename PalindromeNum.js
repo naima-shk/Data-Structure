cae4558e63b81776de599f439077b85b60c521ee
naima shaikh <naimashaikh550@gmail.com>
2020-02-01 23:50:05 +0500

    'solution'

--- PalindromeNum.js
+++ PalindromeNum.js
@@ -1,27 +1,18 @@
-//with tostring
-
-var isPalindrome = function(x) {
-    return true 
+var isPalindrome = function (num) {
+  var numberAux= num
+   var reversedNumber=0
+    
+    while (numberAux>0){
+        var rightDigit=Math.floor(numberAux %10)
+        reversedNumber=reversedNumber *10 +rightDigit
+        numberAux= Math.floor (numberAux/10)
+    }
+    if (num == reversedNumber){
+        return true
+    } else {
+            return false
+    
+  } 
+}
  
- palindrome('121');
- };
-
- function isNumPalindrome(num) {
-    //WITHOUT toString()
-    if (num < 0 || num % 10 === 0 && num !== 0) {
-      return false;
-    }
     
-    var roundUp = function(num, precision) {
-    precision = Math.pow(10, precision);
-    return Math.ceil(num * precision) / precision;
-    };
-    
-    var revertedNum = 0;
-    while (num > revertedNum) {
-      revertedNum = Math.floor(revertedNum * 10 + num % 10);
-      num = Math.floor(num / 10);
-    }
-    return Math.floor(num) === Math.floor(revertedNum) || Math.floor(num) == Math.floor(revertedNum / 10);
-  }
-   isNumPalindrome(121);
